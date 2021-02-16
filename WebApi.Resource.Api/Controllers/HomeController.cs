ae774c832e14777ff3c74564b12f16db7820d40e
ruchit <>
2021-02-16 23:50:41 +0530

    updated unauthorised api

--- WebApi.Resource.Api/Controllers/HomeController.cs
+++ WebApi.Resource.Api/Controllers/HomeController.cs
@@ -1,23 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace WebApi.Resource.Api.Controllers
 {   
     [RoutePrefix("api/home")]
     public class HomeController : ApiController
     {
-        [Authorize]
         [Route("")]
-        public IEnumerable<object> Get()
+        public IHttpActionResult Get()
         {
-            var identity = User.Identity as ClaimsIdentity;
-            return identity.Claims.Select(c => new
-            {
-                Type = c.Type,
-                Value = c.Value
-            });
+            string content = "Hello Worl!";
+            return Ok(content);
         }
     }
 }
